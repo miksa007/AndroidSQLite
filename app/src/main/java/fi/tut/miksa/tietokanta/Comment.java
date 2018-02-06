6e5093e33182fbf4fb63ec4aae099f6d54fced98
miksa <miksa007@gmail.com>
2018-02-06 15:02:55 +0200

    Tietokanta toimii

---
+++ app/src/main/java/fi/tut/miksa/tietokanta/Comment.java
@@ -0,0 +1,32 @@
+package fi.tut.miksa.tietokanta;
+
+/**
+ * Created by miksa on 2/6/18.
+ */
+
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
---
+++ app/src/main/java/fi/tut/miksa/tietokanta/MainActivity.java
@@ -0,0 +1,61 @@
+package fi.tut.miksa.tietokanta;
+
+import android.app.ListActivity;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends ListActivity{//AppCompatActivity {
+    private CommentsDataSource dataSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dataSource=new CommentsDataSource(this);
+        dataSource.open();
+
+        List<Comment> values=dataSource.getAllComments();
+        ArrayAdapter<Comment> adapter=new ArrayAdapter<Comment>(this,android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+
+    }
+
+    public void onClick(View view){
+        ArrayAdapter<Comment> adapter=(ArrayAdapter<Comment>)getListAdapter();
+        Comment comment=null;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
+                int nextInt = new Random().nextInt(3);
+                // save the new comment to the database
+                comment = dataSource.createComment(comments[nextInt]);
+                adapter.add(comment);
+                break;
+            case R.id.delete:
+                if (getListAdapter().getCount() > 0) {
+                    comment = (Comment) getListAdapter().getItem(0);
+                    dataSource.deleteComment(comment);
+                    adapter.remove(comment);
+                }
+                break;
+        }
+    }
+    @Override
+    protected void onResume() {
+        dataSource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        dataSource.close();
+        super.onPause();
+    }
+}
